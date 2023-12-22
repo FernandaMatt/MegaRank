95c32c5ab2627f261071cae779b8ca0d6aef343c
fcaetano <fernandacunha@id.uff.br>
2023-12-22 11:56:49 -0300

    chore: renaming api controller

--- src/controllers/apiControllers.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { User } from '../models/Users';
-import { Sequelize } from 'sequelize';
-
-export const listParticpants = async (req: Request, res: Response) => {
-  let users = await User.findAll();
-
-  res.json({ users });
-};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
-import * as ApiController from '../controllers/apiControllers';
+import { Router } from "express";
+
+import * as ApiController from "../controllers/apiController";
 
 const router = Router();
 
-router.get("/participants", ApiController.listParticpants);
+router.get("/participants", ApiController.listParticipants);
+router.post("/participants", ApiController.createParticipant);
+router.put("/participants/:id", ApiController.updateParticipant);
+router.delete("/participants/:id", ApiController.deleteParticipant);
 
 export default router;
